9d27d979c16aee9131f37a6fb53d6626717591a9
MaulanaDevtech422 <163699226+MaulanaDevtech422@users.noreply.github.com>
2024-03-18 15:30:08 +0700

    Create date.sh

---
+++ date.sh
@@ -0,0 +1,17 @@
+                                                      b.sh                                                         Modified#!/bin/bash
+
+# Display the current date and time
+date | lolcat
+
+#cara install via termux
+# apt update && apt upgrade -y
+# apt install git
+# pkg install nano
+# pkg install ruby
+# gem install lolcat 
+# git clone https://github.com/MaulanaDevtech422/Date
+# ls
+# cd Date
+# chmod+x date.sh
+# ./date.sh
+#selesai selamat mencoba 
